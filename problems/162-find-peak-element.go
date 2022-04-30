eddf3296e581899dbf4f9d09042c95e40b50c531
putcho <nagata.twi@gmail.com>
2022-04-30 19:51:02 +0900

    Add: 162 :tada:

---
+++ problems/162-find-peak-element.go
@@ -0,0 +1,17 @@
+package main
+
+//https://leetcode.com/problems/find-peak-element/
+
+func findPeakElement(nums []int) int {
+	l := 0
+	r := len(nums) - 1
+	for l < r {
+		m := l + (r-l)/2
+		if nums[m] > nums[m+1] {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
