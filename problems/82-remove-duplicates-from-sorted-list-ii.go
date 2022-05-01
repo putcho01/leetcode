3eaebb4fbea9212f3fc1f1e220b7341f1f37fed1
putcho <nagata.twi@gmail.com>
2022-05-01 18:14:08 +0900

    Add: 82 :tada:

---
+++ problems/82-remove-duplicates-from-sorted-list-ii.go
@@ -0,0 +1,32 @@
+package main
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	//Two Pointers
+	var pre *ListNode
+	cur := head
+
+	for cur != nil && cur.Next != nil {
+		if cur.Val != cur.Next.Val {
+			pre = cur
+		} else {
+			for cur.Next != nil && cur.Next.Val == cur.Val {
+				cur = cur.Next
+			}
+			if pre != nil {
+				pre.Next = cur.Next
+			} else {
+				head = cur.Next
+			}
+		}
+		cur = cur.Next
+	}
+
+	return head
+}
