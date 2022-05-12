7e571d89259792be332a703ff5efe2d048115c66
putcho <nagata.twi@gmail.com>
2022-05-13 03:29:26 +0900

    Add 713 :tada:

---
+++ problems/713-subarray-product-less-than-k.go
@@ -0,0 +1,34 @@
+package main
+
+// https://leetcode.com/problems/subarray-product-less-than-k/
+
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	counter := 0
+	left := 0
+
+	for right := 0; right < len(nums); right++ {
+
+		currentProduct := 1
+		left = right + 1
+		currentProduct *= nums[right]
+
+		if currentProduct < k {
+			counter++
+		}
+
+		for currentProduct < k && left < len(nums) {
+
+			currentProduct *= nums[left]
+			left++
+
+			if currentProduct < k {
+				counter++
+			}
+
+		}
+
+	}
+
+	return counter
+
+}
