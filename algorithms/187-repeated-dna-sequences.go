62660b20bccc95bbf630d16f69a78595274126bb
putcho <nagata.twi@gmail.com>
2022-06-09 07:02:00 +0900

    Add: 187 :tada:

---
+++ algorithms/187-repeated-dna-sequences.go
@@ -0,0 +1,23 @@
+package main
+
+//https://leetcode.com/problems/repeated-dna-sequences/
+
+func findRepeatedDnaSequences(s string) []string {
+	counts := map[string]int{}
+	output := []string{}
+
+	for i := 0; i+10 <= len(s); i++ {
+		current := string(s[i : i+10])
+		v, k := counts[current]
+		if k {
+			if v == 1 {
+				output = append(output, current)
+			}
+			counts[current]++
+		} else {
+			counts[current] = 1
+		}
+	}
+
+	return output
+}
