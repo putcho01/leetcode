20539e7e51d1e8354f2d81c17925a5f36eb6235d
putcho01 <53213591+putcho01@users.noreply.github.com>
2022-07-28 14:11:40 +0900

    Add: 704 :tada:

---
+++ algorithms/704-binary-search.go
@@ -0,0 +1,20 @@
+package main
+
+// Binary Search
+// https://leetcode.com/problems/binary-search
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	return -1
+}
