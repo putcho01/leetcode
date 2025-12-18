04efa6d846416b16b57e35017175dce8769816b6
putcho01 <nagattyo.job@gmail.com>
2025-12-18 10:39:22 +0900

    Update 4-SetMismatch.go

--- 2025/Data Structures and Algorithms/4-SetMismatch.go
+++ 2025/Data Structures and Algorithms/4-SetMismatch.go
@@ -9,12 +9,16 @@ func findErrorNums(nums []int) []int {
 
 	for _, num := range nums {
 		if numSet[num] {
+			// すでに出現している値の場合、重複した値として記録
 			duplicate = num
 		} else {
+			// 出現済みの値を記録
 			numSet[num] = true
 		}
 	}
 
+	// 1 から s までの数値の中で出現していない値を探す
+	// それが欠損した値となる
 	for i := 1; i <= s; i++ {
 		if !numSet[i] {
 			missing = i
