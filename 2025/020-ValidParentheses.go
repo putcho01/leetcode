4d03c2daef0114657c4cbd7e0b46763933e481ad
putcho01 <nagattyo.job@gmail.com>
2025-12-31 16:46:22 +0900

    :tada: 優先度キュー

--- 2025/020-ValidParentheses.go
+++ 2025/020-ValidParentheses.go
@@ -8,6 +8,7 @@ func isValid(s string) bool {
 		'(': ')',
 		'[': ']',
 	}
+	// 文字列を1文字ずつ処理
 	for _, c := range s {
 		if char, ok := mapping[c]; ok {
 			stack = append(stack, char)
@@ -15,9 +16,12 @@ func isValid(s string) bool {
 			if len(stack) == 0 || stack[len(stack)-1] != c {
 				return false
 			} else {
+				// 一致 → pop
 				stack = stack[:len(stack)-1]
 			}
 		}
 	}
+	// 全部見終わって、スタックが空なら全部対応取れてる → true
+	// 残ってたら閉じられてない括弧がある → false
 	return len(stack) == 0
 }
